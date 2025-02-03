3ae293daba27c25370d58624045b1ca3beaa6711
Zannatul Fardaush Tripty <65339499+Zannatul007@users.noreply.github.com>
2025-02-03 12:31:50 +0600

    course content 1

---
+++ Stepping Into the World of Python/escapeChar.py
@@ -0,0 +1,28 @@
+print("This is another new thing to know is \t escape characters \n let's dive into this \n this is actually new thing i come to know")
+
+
+anotherSplittedText = """ Consecutive
+ three quotation also
+ helps to split 
+ text automatically 
+ by using only keyboard new lines
+"""
+print(anotherSplittedText)
+
+anotherSplittedText_2 =""" Consecutive \
+ three quotation also \
+ helps to split \
+ text automatically \
+ by using only keyboard new lines but by putting \ it doesn't break
+""" 
+print(anotherSplittedText_2)
+
+print("Number 1 \tThe Larch\nNumber 2\tThe Horse Chestnut")
+
+# print("D:\Users\Python Materials\notes.txt")
+
+#using //
+print('D:\\Users\Python Materials\\notes.txt')
+
+#using raw line
+print(r"D:\Users\Python Materials\notes.txt")
---
+++ Stepping Into the World of Python/print.py
@@ -0,0 +1,9 @@
+print('Python is a fun activity')
+print("Python is my love language")
+print("it can't be comparable with any other language")
+print('I just love the meticulous "descriptions" so much')
+
+name = input("Now you can insert your name ")
+greeting = "Welcome to this heaven"
+print("hello")
+print(greeting + " " +name)
