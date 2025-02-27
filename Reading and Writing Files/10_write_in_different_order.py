4f89c96ce0dd2670b41373c634c7771b24230912
Zannatul Fardaush Tripty <65339499+Zannatul007@users.noreply.github.com>
2025-02-27 10:14:12 +0600

    correction

--- Reading and Writing Files/10_write_in_different_order.py
+++ Reading and Writing Files/10_write_in_different_order.py
@@ -1,9 +1,19 @@
 ordering = ['Country','Gold','Silver','Bronze','Total','Rank']
 import csv
-with open('OlympicMedals_2020.csv','r',encoding='utf-8',newline='') as read_file, open('modified_OlympicMedals_2020.csv','w',encoding='utf-8',newline='') as write_file:
-    csv_read_file = csv.DictReader(read_file,delimiter="|")
-    for row_dict in read_file:
+
+with open ('D:\Python Materials\Reading and Writing Files\OlympicMedals_2020.csv','r',encoding='utf-8',newline='')as read_file,\
+open('ModifiedOlympicMedals.csv','w',encoding='utf-8',newline = '') as write_file:
+
+    csv_read_file = csv.DictReader(read_file)
+    for row in csv_read_file:
         new_dict = {}
         for key in ordering:
-            value = row_dict[key]
+            value = row[key]
+            new_dict[key] = value
+        print(new_dict)
+        print(f"{new_dict}",file=write_file)
 
+
+    
+# for key,index in new_dict.items():
+#     print(key,index)
