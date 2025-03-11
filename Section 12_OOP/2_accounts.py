a0f72f023152544414095b66a44282e04cc4a94c
Zannatul Fardaush Tripty <65339499+Zannatul007@users.noreply.github.com>
2025-03-11 10:23:04 +0600

    formatted nicely

--- Section 12_OOP/2_accounts.py
+++ Section 12_OOP/2_accounts.py
@@ -2,62 +2,58 @@ import datetime
 import pytz
 
 
-class Account:
-    """ Simple account class with balance """
+class Account():
+    def __init__(self,name,balance):
+        self.__name = name 
+        self.__balance = balance
+        self.__transactions_list = [[Account.current_time(),balance]]
 
+        self.show_balance()
     @staticmethod
     def current_time():
-        utc_time = datetime.datetime.utcnow()
-        return pytz.utc.localize(utc_time)
+        time = datetime.datetime.utcnow()
+        return pytz.utc.localize(time)
 
-    def __init__(self, name, balance):
-        self._name = name
-        self.balance = balance
-        self.transaction_list = [(Account.current_time(), balance)]
-        print("Account created for " + self._name)
-        self.show_balance()
-
-    def deposit(self, amount):
-        if amount > 0:
-            self.balance += amount
-            self.show_balance()
-            self.transaction_list.append((Account.current_time(), amount))
-
-    def withdraw(self, amount):
-        if 0 < amount <= self.balance:
-            self.balance -= amount
-            self.transaction_list.append((Account.current_time(), -amount))
-        else:
-            print("The amount must be greater than zero and no more then your account balance")
-        self.show_balance()
-
+    def deposite(self,amount):
+        if(amount>0):
+            self.__balance +=amount
+        self.__transactions_list.append([Account.current_time(),amount])
+        # self.show_balance()
+    def withdraw(self,amount):
+        if(0<amount<=self.__balance):
+            self.__balance-=amount
+        self.__transactions_list.append([Account.current_time(),-amount])
+        # self.show_balance()
     def show_balance(self):
-        print("Balance is {}".format(self.balance))
-
-    def show_transactions(self):
-        for date, amount in self.transaction_list:
-            if amount > 0:
-                tran_type = "deposited"
+        print('The account has {}'.format(self.__balance))
+    def manage_transactions(self):
+        for date, value in self.__transactions_list:
+            if(value >0):
+                tran_type = 'deposited'
             else:
-                tran_type = "withdrawn"
-                amount *= -1
-            print("{:6} {} on {} (local time was {})".format(amount, tran_type, date, date.astimezone()))
+                tran_type = 'withdrawn'
+                value *=-1
+            print("{:6} {} on {} (local time was {})".format(value, tran_type, date, date.astimezone()))
+
+        
+        
+if __name__ =='__main__':
+    zane = Account('zane',800)
+    # zane.show_balance()
+    zane.deposite(200)
+    zane.withdraw(100)
+    zane.deposite(300)
+
+    zane.manage_transactions()
+    zane.show_balance()
 
 
-if __name__ == '__main__':
-    tim = Account("Tim", 0)
+    tim = Account('Tim',0)
+    tim.balance = 200
+    tim.deposite(300)
+    tim.withdraw(100)
+    tim.deposite(500)
+    tim.manage_transactions()
     tim.show_balance()
-    tim.deposit(1000)
-    tim.withdraw(500)
-    tim.withdraw(2000)
-    tim.show_transactions()
+    print(tim.__dict__)
 
-    # steph = Account("Steph", 800)
-    # steph.balance = 200
-    # steph.deposit(100)
-    # steph.withdraw(200)
-    # steph.show_transactions()
-    # steph.show_balance()
-    # print(steph.__dict__)
-    # steph._Accountbalance = 40
-    # steph.show_balance()
--- library_management/library_management_2.py
+++ library_management/library_management_2.py
@@ -1,85 +1,101 @@
 import json
-# book_id = 0 
+
+# book_id = 0
 # books_collection = {}
 
 try:
-    with open('library_data.json', 'r', encoding='utf-8') as read_file:
+    with open(
+        "D:\Python Materials\library_management\library_data.json",
+        "r",
+        encoding="utf-8",
+    ) as read_file:
         books_collection = json.load(read_file)
         book_id = len(books_collection)
-except (json.JSONDecodeError):
-    books_collection = {} 
-    book_id = 0 
+except json.JSONDecodeError:
+    books_collection = {}
+    book_id = 0
 
-def add_book(title:str,author:str):
+
+def add_book(title: str, author: str):
     global book_id
-    book_id+=1
-    books_collection[book_id]={
-        'title':title,
-        'author':author,
-        'status':'Available'
+    book_id += 1
+    books_collection[book_id] = {
+        "title": title,
+        "author": author,
+        "status": "Available",
     }
 
-def check_out(id1:int,b_name:str,d_date:str):
-    if(id1 in books_collection):
-        if(books_collection[id1]['status']=='Available'):
-            books_collection[id1]['status']='Checked out'
-            books_collection[id1]['b_name']= b_name
-            books_collection[id1]['d_date']= d_date
-            print('Book checked out successfully!')
+
+def check_out(id1: int, b_name: str, d_date: str):
+    if id1 in books_collection:
+        if books_collection[id1]["status"] == "Available":
+            books_collection[id1]["status"] = "Checked out"
+            books_collection[id1]["b_name"] = b_name
+            books_collection[id1]["d_date"] = d_date
+            print("Book checked out successfully!")
         else:
             print(f"Book was checked out by {books_collection[id1]['b_name']} in past")
     else:
         print("Book doesn't exist")
-   
-    
-def return_book(id1:int):
-    if (id1 in books_collection):
-        if(books_collection[id1]['status'] == 'Checked out'):
-            books_collection[id1]['status'] = 'Available'
-            print('Book returned successfully')
+
+
+def return_book(id1: int):
+    if id1 in books_collection:
+        if books_collection[id1]["status"] == "Checked out":
+            books_collection[id1]["status"] = "Available"
+            print("Book returned successfully")
         else:
-            print('Book is not checked out')
+            print("Book is not checked out")
     else:
-        print('Book never exists in library')
+        print("Book never exists in library")
+
 
 def view_status():
-    for id1,book_info in books_collection.items():
-        if(book_info['status']=='Available'):
-            print(f"ID: {id1}, Title: {book_info['title']}, Author: {book_info['author']}, Status: {book_info['status']}")
+    for id1, book_info in books_collection.items():
+        if book_info["status"] == "Available":
+            print(
+                f"ID: {id1}, Title: {book_info['title']}, Author: {book_info['author']}, Status: {book_info['status']}"
+            )
         else:
-            print(f"ID: {id1}, Title: {book_info['title']}, Author: {book_info['author']}, Status: {book_info['status']} by {book_info['b_name']}")
+            print(
+                f"ID: {id1}, Title: {book_info['title']}, Author: {book_info['author']}, Status: {book_info['status']} by {book_info['b_name']}"
+            )
 
 
-choice = ''
-choice_list = ['Add Book','Check Out Book','Return Book','View Library Status','Exit']
+choice = ""
+choice_list = [
+    "Add Book",
+    "Check Out Book",
+    "Return Book",
+    "View Library Status",
+    "Exit",
+]
+for index, item in enumerate(choice_list):
+    print(f"{index+1}:\t{item}")
 
+while True:
+    choice = input("Enter your choice: ")
+    if choice == "1":
+        title = input("Enter book title: ")
+        author = input("Enter author: ")
+        add_book(title, author)
+        print("Book added successfully!")
 
-for index,item in enumerate(choice_list):
-    print(f'{index+1}:\t{item}')
-
-while(choice != 5):
-
-    choice = int(input('Enter your choice: '))
-
-    if(choice == 1):
-        title = input('Enter book title: ')
-        author = input('Enter author: ')
-        add_book(title,author)
-        print('Book added successfully!')
-
-    elif(choice == 2):
-        b_id = int(input('Enter book ID to check out: '))
+    elif choice == "2":
+        b_id = int(input("Enter book ID to check out: "))
         b_name = input("Enter borrower's name: ")
         d_date = input("Enter due date (YYYY-MM-DD): ")
-        check_out(b_id,b_name,d_date)
+        check_out(b_id, b_name, d_date)
 
-    elif(choice == 3):
-        b_id1 = int(input('Enter book ID to return: '))
+    elif choice == "3":
+        b_id1 = int(input("Enter book ID to return: "))
         return_book(b_id1)
 
-    elif(choice == 4):
+    elif choice == "4":
         view_status()
 
-with open('library_data.json','w',encoding='utf-8') as json_file:
-    json.dump(books_collection,json_file)
+    elif choice == "5":
+        break
 
+with open("library_data.json", "w", encoding="utf-8") as json_file:
+    json.dump(books_collection, json_file)
