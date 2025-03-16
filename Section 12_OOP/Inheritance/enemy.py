01be297e8b317cb735ec45fd67b69229882cfb58
Zannatul Fardaush Tripty <65339499+Zannatul007@users.noreply.github.com>
2025-03-16 15:27:41 +0600

    villain

--- Section 12_OOP/Inheritance/enemy.py
+++ Section 12_OOP/Inheritance/enemy.py
@@ -1,22 +1,42 @@
 class Enemy:
-    def __init__(self, name, hit_points, lives):
+    def __init__(self, name="Enemy", hit_points=0, lives=1):
         self.name = name
         self.hit_points = hit_points
         self.lives = lives
+        self.alive = True
 
     def take_damage(self, damage):
         remaining_points = self.hit_points - damage
         if remaining_points >= 0:
             print(
-                "I took {} point and remaining points {}".format(
-                    damage, remaining_points
+                "{} took {} point and remaining points {}".format(
+                    self.name, damage, remaining_points
                 )
             )
             self.hit_points = remaining_points
         else:
             self.lives -= 1
+
             if self.lives <= 0:
+                self.alive = False
                 print("Game Over")
+            else:
+                print(f"{self.name} lost a life")
 
     def __str__(self):
-        return "Name {0.name}, Hit points {0.hit_points}, Lives {0.lives}".format(self)
+        return "Name: {0.name}, Hit points: {0.hit_points}, Lives: {0.lives}".format(
+            self
+        )
+
+
+class Troll(Enemy):
+    def __init__(self, name="Troll Enemy", hit_points=0, lives=1):
+        super().__init__(name, hit_points, lives)
+
+    def grunt(self):
+        print(f"{self.name} kicked {self.name}")
+
+
+class Vampire(Enemy):
+    def __init__(self, name="Vampire Enemy", hit_points=0, lives=1):
+        super().__init__(name, hit_points, lives)
--- Section 12_OOP/Inheritance/main.py
+++ Section 12_OOP/Inheritance/main.py
@@ -1,7 +1,25 @@
-from enemy import Enemy
+from enemy import Enemy, Troll, Vampire
 
-random_enemy = Enemy("Sagacious", 100, 1)
-random_enemy.take_damage(50)
-print(random_enemy)
-random_enemy.take_damage(60)
-print(random_enemy)
+# random_enemy = Enemy("Sagacious", 100, 1)
+# random_enemy.take_damage(50)
+# print(random_enemy)
+# random_enemy.take_damage(60)
+# print(random_enemy)
+
+troll_1 = Troll()
+print(troll_1)
+
+another_troll = Troll("Ugly_another", 20, 1)
+print("Another troll --> {}".format(another_troll))
+
+troll_2 = Troll("Ugly Brother", 24)
+print("The third Troll --> {}".format(troll_2))
+troll_2.grunt()
+
+
+vampire = Vampire("Zane", 30, 5)
+
+while vampire.alive:
+    vampire.take_damage(5)
+    print(vampire)
+
