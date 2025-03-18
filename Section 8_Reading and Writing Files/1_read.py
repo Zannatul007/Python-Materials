4f0312e3d8686d4063d464e2d4aa32e966b40aff
Zannatul Fardaush Tripty <65339499+Zannatul007@users.noreply.github.com>
2025-03-18 15:17:56 +0600

    last code of oop module

--- Section 8_Reading and Writing Files/1_read.py
+++ Section 8_Reading and Writing Files/1_read.py
@@ -1,25 +1,27 @@
-#file path = D:\Python Materials\Reading and Writing Files\jabber.txt
+# file path = D:\Python Materials\Reading and Writing Files\jabber.txt
 # jabber = open("D:\Python Materials\Reading and Writing Files\jabber.txt",'r')
 # for line in jabber:
 #     print(line.strip())
 #     print(line,end = "")
 
-#using 'with'
+# using 'with'
 # with open("D:\Python Materials\Reading and Writing Files\jabber.txt","r") as jabber:
 #     for line in jabber:
 #         print(line.rstrip())
 
-#Read file by read,readline and readlines
-#Readlines -> works on line
+# Read file by read,readline and readlines
+# Readlines -> works on line
 
-with open('.\Reading and Writing Files\jabber.txt') as jabber:
-     
-     lines = jabber.readlines()
+# path (terminal path + file path)
+# terminal path can be obtained by typing 'pwd' in terminal
+with open(".\Reading and Writing Files\jabber.txt") as jabber:
+
+    lines = jabber.readlines()
 
 for line in reversed(lines):
-    print(line,end='')
+    print(line, end="")
 
-# #Read -> works on string 
+# #Read -> works on string
 # with open("D:\Python Materials\Reading and Writing Files\jabber.txt","r") as jabber:
 #     lines_with_read = jabber.read()
 # for line in reversed(lines_with_read):
