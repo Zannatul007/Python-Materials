044ec2a3b7a163c8d77557768429ae4509cd2471
Zannatul Fardaush Tripty <65339499+Zannatul007@users.noreply.github.com>
2025-03-20 15:47:09 +0600

    search

--- Section 12_OOP/Library Management OOP/book_management.py
+++ Section 12_OOP/Library Management OOP/book_management.py
@@ -2,7 +2,7 @@ class Admin:
     def __init__(self, name):
         self.books_collection = {}
 
-    def add_book(self, title, author, genre, ISBN, copies: int):
+    def add_book(self, title, author, genre, ISBN: str, copies: int):
         self.books_collection[ISBN] = {
             "isbn": ISBN,
             "title": title,
@@ -18,9 +18,16 @@ class Admin:
     def delete_book(self, isbn):
         self.books_collection.pop(isbn)
 
-    def search_book(self, search_criteria):
-        for key,book_info in self.books_collection.items():
-            if(book_info[])
+    def search_book(self, search_item):
+        pos = None
+        for key1, book_info in self.books_collection.items():
+            for key2, value in book_info.items():
+                if value == search_item:
+                    pos = key1
+                    break
+            if pos:
+                break
+        print("The details of the book is {}".format(self.books_collection[pos]))
 
     def book_storage(self):
         for key, book_info in self.books_collection.items():
